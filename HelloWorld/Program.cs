edd40f5543f8a4bd660232837b2c19b32254422b
TimoW1989 <timo@familie-wensing.de>
2022-04-09 20:33:53 +0200

    Added switch block!

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -64,5 +64,50 @@ else
 {
     Console.WriteLine("Du darfst nicht in den Kinosaal, tut mir Leid!");
 }
+Console.WriteLine();
+
+//Switch Block
+
+int monat = 9;
+
+switch (monat)
+{
+    case 1:
+        Console.WriteLine("Januar");
+        break;
+    
+    case 2:
+        Console.WriteLine("Februar");
+        break;
+
+    case 3:
+        Console.WriteLine("März");
+        break;
+
+    case 4:
+        Console.WriteLine("April");
+        break;
+    
+    case 5:
+        Console.WriteLine("Mai");
+        break;
+
+    case 6:
+        Console.WriteLine("Juni");
+        break;
+
+    case 7:
+        Console.WriteLine("Juli");
+        break;
+
+    case 8:
+        Console.WriteLine("August");
+        break;
+        
+    default:
+        Console.WriteLine("Ungültig!");
+        break;
+}
+
 Console.ReadKey();
 

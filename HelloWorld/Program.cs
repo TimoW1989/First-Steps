ea8af76c8a94a8c080e2e703e33b8739751a8f3f
TimoW1989 <timo@familie-wensing.de>
2022-04-09 19:34:11 +0200

    Added some code!

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,5 +4,16 @@ Console.WriteLine("Hello World!");
 Console.WriteLine("Hallo Welt!");
 Console.WriteLine("Mein Name ist Timo!");
 Console.WriteLine("Dieses ist mein erstes C# Projekt!");
+
+//Variablen und Datentypen
+int alter = 33;
+
+Console.WriteLine("Ich bin " + alter + " Jahre alt!");
+
+string name = "Klaus";
+double grösse = 1.72;
+decimal konto = 4321.45M;
+
+Console.WriteLine(name + " ist " + grösse + "m groß und sein Kontostand beträgt " + konto + " Euro!");
 Console.ReadKey();
 

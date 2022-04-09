d5602d8b679d35457aafaf8f5ab641a8d152c05a
timo <timo@DESKTOP-82F113M>
2022-04-09 19:11:49 +0200

    Projektdateien hinzufügen.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hallo Welt!");
+Console.WriteLine("Mein Name ist Timo!");
+Console.WriteLine("Dieses ist mein erstes C# Projekt!");
+Console.ReadKey();
+
